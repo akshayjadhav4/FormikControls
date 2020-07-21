053b81142d77acac65054518c07413c4312c3766
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-21 16:08:35 +0530

    return Input component in switch

--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Input from "./Input/Input";
 
 //decides which form field to render dependingon passed prop.
-function FormikControl({ control }) {
+function FormikControl({ control, ...rest }) {
   switch (control) {
     case "input":
+      return <Input {...rest} />;
     case "textarea":
     case "select":
     case "radio":
