281db1fd002e2d00eec2b95e21bf7d09fb411941
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-21 17:13:11 +0530

    FormikCOntrol used for selectOption

--- src/components/FormikContaier.js
+++ src/components/FormikContaier.js
@@ -3,14 +3,24 @@ import * as Yup from "yup";
 import { Formik, Form } from "formik";
 import FormikControl from "./FormikControl";
 function FormikContaier() {
+  const dropdownOptions = [
+    {key : 'Choose your option',value:' '},
+    {key : 'Option 1',value : '1'},
+    {key : 'Option 2',value : '2'},
+    {key : 'Option 3',value : '3'},
+    {key : 'Option 4',value : '4'},
+    {key : 'Option 5',value : '5'},
+  ]
   const initialValues = {
     email : "",
     description : "",
+    selectOption : ''
   };
 
   const validationSchema = Yup.object({
     email : Yup.string().email('Inavalid email format').required('Required'),
-    description : Yup.string().required('Required')
+    description : Yup.string().required('Required'),
+    selectOption : Yup.string().required('Required')
   });
 
   const onSubmit = (values) => {
@@ -36,6 +46,7 @@ function FormikContaier() {
             label="Description"
             name="description"
           />
+          <FormikControl  control='select'  label='Select Option' name='selectOption' options={dropdownOptions}/>
           <button type="submit" className="waves-effect waves-light btn">
             Submit
           </button>
