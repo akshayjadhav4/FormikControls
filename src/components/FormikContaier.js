dccef22f83728e68fd9ddcaf35b96eba2e87ce1e
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-21 16:09:08 +0530

    FormikControl component used for email field

--- src/components/FormikContaier.js
+++ src/components/FormikContaier.js
@@ -1,10 +1,15 @@
 import React from "react";
 import * as Yup from "yup";
 import { Formik, Form } from "formik";
+import FormikControl from "./FormikControl";
 function FormikContaier() {
-  const initialValues = {};
+  const initialValues = {
+    email : ""
+  };
 
-  const validationSchema = Yup.object({});
+  const validationSchema = Yup.object({
+    email : Yup.string().email('Inavalid email format').required('Required')
+  });
 
   const onSubmit = (values) => {
     console.log("Form Data", values);
@@ -18,6 +23,12 @@ function FormikContaier() {
     >
       {(formik) => (
         <Form>
+          <FormikControl
+            control="input"
+            type="email"
+            label="Email"
+            name="email"
+          />
           <button type="submit" className="waves-effect waves-light btn">
             Submit
           </button>
