e3d839c98c8c00c8929f29401e5b46e1c8240aff
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-21 18:19:02 +0530

    CheckboxGroup component

---
+++ src/components/Checkbox Group/CheckboxGroup.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+import TextError from "../ErrorComponent/TextError";
+
+function CheckboxGroup(props) {
+  const { label, name, options, ...rest } = props;
+  return (
+    <div className="">
+      <label>{label}</label>
+      <Field name={name} {...rest}>
+        {({ field }) => {
+          //   console.log(field);
+          return options.map((option) => {
+            return (
+              <p key={option.key}>
+                <label htmlFor={option.value}>
+                  <input
+                    id={option.value}
+                    type="checkbox"
+                    {...field}
+                    value={option.value}
+                    checked={field.value.includes(option.value)}
+                  />
+                  <span>{option.key}</span>
+                </label>
+              </p>
+            );
+          });
+        }}
+      </Field>
+      <ErrorMessage name={name} component={TextError} />
+    </div>
+  );
+}
+
+export default CheckboxGroup;
